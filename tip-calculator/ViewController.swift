baa1452db2fd58be1f6fb24fcf16e0940a26417d
Yani Buchkov <ybuchkov@gmail.com>
2024-03-15 11:32:36 +0200

    🚀 Setup Resources & Package manager

---
+++ tip-calculator/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by Yani Buchkov on 15.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
+
